372bff4cb8680e63ebce00601cdd6390273929e7
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2025-10-02 20:58:38 -0300

    create dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  no: number;
+}
